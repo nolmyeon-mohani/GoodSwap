e319bbb0d018e7e8b2a5d0568975713f6e88d490
SJ0503 <didtpwls3857@gmail.com>
2024-12-05 23:16:42 +0900

    Initial commit

---
+++ src/main/java/com/mohani/nolmyeon/NolmyeonApplication.java
@@ -0,0 +1,13 @@
+package com.mohani.nolmyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolmyeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NolmyeonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohani/nolmyeon/NolmyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohani.nolmyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolmyeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
